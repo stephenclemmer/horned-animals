835ef78bd49c842a63fd67a73f01557cb82c406e
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-08-16 21:05:50 -0700

    Displayed images from JSON file with data being passed via props

--- src/Main.js
+++ src/Main.js
@@ -2,20 +2,42 @@ import React from 'react';
 import Grid from './Grid.js';
 import HornedBeast from './Horned-Beast.js';
 
+import animals from './data.json';
+
+
+
 class Main extends React.Component{
   
+  //Create state within the constructor function 
+  constructor(props){
+    super(props);
+    // Create state tied to the animals JSON file
+    this.state = {array: animals}
+  }
 
   render(){
     return(
       <main>
+        {
+        this.state.array.map((value) => 
         <HornedBeast 
-          title={"Unicorn"} 
-          imageUrl={"https://via.placeholder.com/150"} 
-          description={"Unicorn from Narnia"}/>
-        <HornedBeast 
-          title={"Bull"} 
-          imageUrl={"https://via.placeholder.com/150"} 
-          description={"Bull from Spain"}/>
+          title={value.title} 
+          imageUrl={value.image_url} 
+          description={value.description}
+          />
+    )
+  }
+
+
+
+
+
+
+{/* 
+<HornedBeast 
+  title={"Bull"} 
+  imageUrl={"https://via.placeholder.com/150"} 
+  description={"Bull from Spain"}/> */}
       </main>
     )
   }
